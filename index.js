005d711e30b2ce3c02bd4c2be7863555d70fceae
Jason Finch <jason.finch@gmail.com>
2022-05-06 11:45:02 +1000

    chore: delete

--- index.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var uuid=require("uuid");function generateUUID(){return"t"+uuid.v4()}console.log(generateUUID()),exports.generateUUID=generateUUID;
-//# sourceMappingURL=index.js.map
