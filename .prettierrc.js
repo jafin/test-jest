dc6874cf232a772de0c811d11fdd26238caad44c
Jason Finch <jason.finch@gmail.com>
2022-05-07 12:27:23 +1000

    configs

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  tabWidth: 2,
+  printWidth: 80,
+  semi: true,
+  bracketSpacing: true,
+  endOfLine: 'lf'
+};
